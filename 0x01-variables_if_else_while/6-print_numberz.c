59e318fa780850f8b252c4edd348f563afbf4307
tracyandesia <uitracers@gmail.com>
2023-06-16 09:04:02 -0700

    added  6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	for (; a < 10 ; a++)
+		putchar('0' + a);
+	putchar('\n');
+	return (0);
+}
