67625eabf106f4c14552fa748e8e8ced26657e69
tracyandesia <uitracers@gmail.com>
2023-06-16 09:35:44 -0700

    added 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 48; n <= 56; a++)
+	{
+		for (m = 49; b <= 57; m++)
+		{
+			if (b > a)
+			{
+				putchar(a);
+				putchar(b);
+				if (a != 56 || b != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
