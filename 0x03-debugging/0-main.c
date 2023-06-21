907381708041270843f48bdd04a7454bf1183dbc
tracyandesia <uitracers@gmail.com>
2023-06-21 08:13:51 -0700

    refactor

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,12 +3,13 @@
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
-int main(void)
+int positive_or_negative(int i)
 {
-	int i;
-
-	i = 0;
-	positive_or_negative(i);
-
-	return (0);
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
 }
+	
