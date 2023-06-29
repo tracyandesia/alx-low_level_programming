5c7812ecc77f35003970e4edc61d840fea578493
tracyandesia <uitracers@gmail.com>
2023-06-29 13:13:41 -0700

    refactor

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,21 +1,23 @@
 #include "main.h"
+
 /**
- * reverse_array - reverse array of integers
- * @a: array
- * @n: number of elements of array
+ * reverse_array - reverses the contents of an array
+ * @a: integer array to be reversed
+ * @n: length of array a
  *
  * Return: void
  */
 void reverse_array(int *a, int n)
 {
-	int i;
-	int t;
+	int i = 0, t;
 
-	for (i = 0; i < n--; i++)
+	n = n - 1;
+	while (i < n)
 	{
-		t = a[i];
-		a[i] = a[n];
-		a[n] = t;
+		t = *(a + i);
+		*(a + i) = *(a + n);
+		*(a + n) = t;
+		i++;
+		n--;
 	}
 }
-
