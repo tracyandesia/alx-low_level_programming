6a21e75f9151baeec6e1d05a54a57c2b362c5ca1
tracyandesia <uitracers@gmail.com>
2023-07-13 09:13:10 -0700

    refactor

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
-#define min(x, y) (((x) < (y)) ? (x) : (y))
-
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
