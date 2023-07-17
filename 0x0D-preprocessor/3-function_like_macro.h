163d48a266f101013b3cda9f7881969fbc372dbe
tracyandesia <uitracers@gmail.com>
2023-07-17 14:36:14 -0700

    refactor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTION_LIKE_MACRO_H_
 #define FUNCTION_LIKE_MACRO_H_
 
-#define ABS(x) ((x) < 0 ? (-(x) : (x))
-typedef int make_iso_compilers_happy;
+#define ABS(x) ((x) < (0) ? -(x) : (x))
+
 
 #endif /* FUNCTION_LIKE_MACRO_H_ */
