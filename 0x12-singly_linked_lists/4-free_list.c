b2ab78e359eac8659ee8b3ff11447502e3bdbd92
tracyandesia <uitracers@gmail.com>
2023-07-27 11:35:18 -0700

    refactor

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,13 +1,20 @@
 #include "lists.h"
-#include <stdlib.h>
+
 /**
-  * free_listint - define function
-  * @head: describe argument
-  */
-void free_listint(listint_t *head)
+ * free_list - frees a list_t list
+ * @head: head of list_t
+ */
+void free_list(list_t *head)
 {
-	if (head == NULL)
-		return;
-	free_listint(head->next);
-	free(head);
+	list_t *current = head;
+	list_t *next;
+
+	while (current)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+	head = NULL;
 }
