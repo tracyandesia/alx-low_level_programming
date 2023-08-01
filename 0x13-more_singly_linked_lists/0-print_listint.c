c9cc4a69c9c1c65c274c28966186fa8defac59e6
tracyandesia <uitracers@gmail.com>
2023-08-01 13:38:39 -0700

    debugging

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,14 +3,13 @@
 /**
  * print_listint - prints all the elements of a listint_t list
  * @g: pointer to head of list
- * Return : number of elements
+ * Return: number of elements
  */
-
 size_t print_listint(const listint_t *g)
 {
 	register size_t count = 0;
 
-	while(g)
+	while (g)
 	{
 		printf("%i\n", g->n);
 		g = g->next;
