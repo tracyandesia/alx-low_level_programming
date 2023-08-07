828b12d5a31d6d11a4b99a3f426d04bbff6f8d3a
tracyandesia <uitracers@gmail.com>
2023-08-07 14:15:39 -0700

    refactor

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,11 +1,10 @@
 #include "elf.h"
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <fcntl.h>
+#include <unistd.h>
 
 void check_elf(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
