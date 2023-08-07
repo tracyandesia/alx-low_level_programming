247851c843ce0c36693217a2d4cc972e1c67d9fb
tracyandesia <uitracers@gmail.com>
2023-08-07 12:30:26 -0700

    refactor

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,9 @@
 #ifndef _MAIN_H_
-#define _MAIN_H
+#define _MAIN_H_
 
 #include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char);
-#endif /*_MAIN_H*/
+
+#endif /*_MAIN_H_*/
